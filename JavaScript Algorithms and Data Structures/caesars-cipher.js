22687d7c23c5580dad0c326b385a4dc1f71b19ac
Tom Kelly <107217874+tom-kelly25@users.noreply.github.com>
2022-08-22 22:10:01 +0100

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/caesars-cipher.js
@@ -0,0 +1,24 @@
+//Caesars Cipher
+
+var lookup = {
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M'
+};
+
+function rot13(encodedStr) {
+  var codeArr = encodedStr.split("");  
+  var decodedArr = [];
+  decodedArr = codeArr.map(function(letter) {
+    if(lookup.hasOwnProperty(letter)) {
+      letter = lookup[letter];
+    }
+    return letter;
+  });
+
+  return decodedArr.join("");
+}
