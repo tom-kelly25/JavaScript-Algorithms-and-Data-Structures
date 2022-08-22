e7eb5131be848f05f5f7de58b60be0f63f3d0b00
Tom Kelly <107217874+tom-kelly25@users.noreply.github.com>
2022-08-22 22:12:32 +0100

    Delete JavaScript Algorithms and Data Structures directory

--- JavaScript Algorithms and Data Structures/telephone-number-validator.js
+++
@@ -1,8 +0,0 @@
-//Telephone Number Validator
-
-function telephoneCheck(str) {
-   const regex = /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/
-   return regex.test(str)
-}
-
-telephoneCheck("555-555-5555");
